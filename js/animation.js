08b38e38d0b66320b7f147bcc6920ad7863eaa7c
Nastya Faizulina <just-fy@mail.ru>
2019-04-09 00:05:22 +0300

    change header, make toggle round button for form

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,21 @@
+const changeButton = () => {
+    const button = document.querySelector('.button-toggle');
+    button.classList.toggle('button-toggle--active');
+};
+
+function toggleForm(){
+    const button = document.querySelector('.button-toggle');
+    if(button.classList.contains('button-toggle--active')){
+        console.log(2);
+        document.getElementById('form').style.width = '100%';
+        document.getElementById('form').style.height = '400px';
+    }
+    else{
+        document.getElementById('form').style.width = '0px';
+    }
+}
+
+
 const getHeight = elem => {
     elem.style.display = 'block'; // make element visible
     const height = elem.scrollHeight + 'px';
@@ -26,7 +44,6 @@ const hideElem = elem => {
         elem.classList.remove('is-visible');
     }, 350);
 };
-
 const toggle = elem => {
     if (elem.classList.contains('is-visible')) {
         hideElem(elem);
