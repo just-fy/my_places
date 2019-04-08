2fd95276475ed373b5e3070498a0b3d3ed110132
Nastya Faizulina <just-fy@mail.ru>
2019-04-08 18:57:54 +0300

    add toggle button for form

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,45 @@
-function openForm() {
-    const buttonHeader = document.getElementById("form");
-    if (buttonHeader.style.display === "none") {
-        buttonHeader.style.display = "block";
-    } else {
-        buttonHeader.style.display = "none";
+const getHeight = elem => {
+    elem.style.display = 'block'; // make element visible
+    const height = elem.scrollHeight + 'px';
+    elem.style.display = ''; // hide element
+    return height;
+};
+
+const showElem = elem => {
+    const height = getHeight(elem);
+    elem.classList.add('is-visible');
+    elem.style.height = height;
+
+    window.setTimeout( () => {
+        elem.style.height = '';
+    }, 350);
+};
+
+const hideElem = elem => {
+    elem.style.height = getHeight(elem);
+
+    window.setTimeout( () => {
+        elem.style.height = '0';
+    }, 1);
+
+    window.setTimeout( () => {
+        elem.classList.remove('is-visible');
+    }, 350);
+};
+
+const toggle = elem => {
+    if (elem.classList.contains('is-visible')) {
+        hideElem(elem);
+        return;
     }
-}
+    showElem(elem);
+};
+
+document.addEventListener('click', (event) => {
+   if (!event.target.classList.contains('header__button-toggle')) return;
+   event.preventDefault();
+   const content = document.querySelector('.form');
+   if (!content) return;
+
+   toggle(content);
+}, false);
--- js/httpclient.js
+++ js/httpclient.js
@@ -1,36 +1,23 @@
-//const mydata = import('./markers');
-const obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
-console.log(obj);
-console.log(1);
+const getJSON = function(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        const status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
 
-// var xhr = new XMLHttpRequest();
-// xhr.open("POST", '../markers.json', true);
-//
-// //Send the proper header information along with the request
-// xhr.setRequestHeader("Content-Type", "application/");
-//
-// xhr.onreadystatechange = function() { // Call a function when the state changes.
-//     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-//         // Request finished. Do processing here.
-//     }
-// };
-// xhr.send("foo=bar&lorem=ipsum");
-// xhr.send(new Int8Array());
-// xhr.send(document);
-
-// function readJSON(path) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', path, true);
-//     xhr.responseType = 'blob';
-//     xhr.onload = function(e) {
-//         if (this.status == 200) {
-//             var file = new File([this.response], 'temp');
-//             var fileReader = new FileReader();
-//             fileReader.addEventListener('load', function(){
-//                 //do stuff with fileReader.result
-//             });
-//             fileReader.readAsText(file);
-//         }
-//     }
-//     xhr.send();
-// }
+getJSON('http://84.201.145.232:8080/places',
+    function(err, data) {
+        if (err !== null) {
+            console.log(false);
+        } else {
+            console.log(true);
+        }
+    });
